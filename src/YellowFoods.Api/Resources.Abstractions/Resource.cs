869f66a9c5b4998707d1fab5279ab75f4d63acab
mar1usz <62397363+mar1usz@users.noreply.github.com>
2021-05-06 19:14:46 +0200

    fix formatting

--- src/YellowFoods.Api/Resources.Abstractions/Resource.cs
+++ src/YellowFoods.Api/Resources.Abstractions/Resource.cs
@@ -6,12 +6,10 @@ namespace YellowFoods.Api.Resources.Abstractions
     public abstract class Resource
     {
         public List<Link> Links { get; set; } = new List<Link> { };
-
         public void AddLink(Link link)
         {
             Links.Add(link);
         }
-
         public void AddLinks(IEnumerable<Link> links)
         {
             Links.AddRange(links);
