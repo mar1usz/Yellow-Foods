d92f675eeff3239fbc039a3d4011e0aaaa770eb5
mar1usz <62397363+mar1usz@users.noreply.github.com>
2021-05-22 19:35:42 +0200

    use foreach (like in ILinkService<>)

--- src/YellowFoods.Api/Resources.Abstractions/Resource.cs
+++ src/YellowFoods.Api/Resources.Abstractions/Resource.cs
@@ -5,14 +5,17 @@ namespace YellowFoods.Api.Resources.Abstractions
 {
     public abstract class Resource
     {
-        public List<Link> Links { get; set; } = new List<Link> { };
+        public IList<Link> Links { get; set; } = new List<Link> { };
         public void AddLink(Link link)
         {
             Links.Add(link);
         }
         public void AddLinks(IEnumerable<Link> links)
         {
-            Links.AddRange(links);
+            foreach (var link in links)
+            {
+                AddLink(link);
+            }
         }
     }
 }
